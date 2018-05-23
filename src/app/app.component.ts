882e94e91df1c7fd2f1634fd1912895772b3ce5f
Jason Dailey <jason.dailey@skuvault.com>
2018-05-23 13:33:35 -0400

    tutorial complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,37 @@
 import { Component } from '@angular/core';
-// import { Component, trigger, animate, style, transition, keyframes } from '@angular/core';
+import { trigger, animate, style, transition, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  // animations: [
-  //   trigger('moveInLeft', [
-  //     transition('void=> *', [style({transform: 'translateX(300px)'}),
-  //       animate(200, keyframes([
-  //        style({transform: 'translateX(300px)'}),
-  //        style({transform: 'translateX(0)'})
-  //     ]))]),
-  //   transition('*=>void', [ style({transform: 'translateX(0px)'}),
-  //       animate(100, keyframes([
-  //         style({transform: 'translateX(0px)'}),
-  //         style({transform: 'translateX(300px)'})
-  //     ]))])
-  //   ])
-  // ]
+  animations: [
+    trigger('moveInLeft', [
+      transition('void=> *', [style({transform: 'translateX(300px)'}),
+        animate(200, keyframes([
+         style({transform: 'translateX(300px)'}),
+         style({transform: 'translateX(0)'})
+      ]))]),
+    transition('*=>void', [ style({transform: 'translateX(0px)'}),
+        animate(100, keyframes([
+          style({transform: 'translateX(0px)'}),
+          style({transform: 'translateX(300px)'})
+      ]))])
+    ])
+  ]
 })
 
 export class AppComponent {
   todoArray = [];
+
   addTodo(value) {
-    this.todoArray.push(value);
-    console.log(this.todoArray);
+    if (value !== '') {
+      this.todoArray.push(value);
+    } else {
+      alert('Field required **');
+    }
   }
 
-  /*delete item*/
   deleteItem(todo) {
     for (let i = 0; i <= this.todoArray.length; i++) {
       if (todo === this.todoArray[i]) {
@@ -38,9 +41,8 @@ export class AppComponent {
   }
 
   todoSubmit(value: any) {
-    if (value !== '') {
+    if (value.todo !== '') {
       this.todoArray.push(value.todo);
-      //this.todoForm.reset()
     } else {
       alert('Field required **');
     }
